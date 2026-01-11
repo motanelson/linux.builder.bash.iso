05fe52769853d6cb317a6177fc144426f94b2720
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-11 19:36:06 +0000

    Add files via upload

--- builder.sh
+++ builder.sh
@@ -1,26 +1,26 @@
 printf "\033c\n "
 
-                mkdir ./tmp/root 2> /dev/null
-                printf "y\n" | rm ./tmp/root/*
-                printf "y\n" | rm ./tmp/root/*.*
+                mkdir /tmp/root 2> /dev/null
+                printf "y\n" | rm /tmp/root/* 2> /dev/null
+                printf "y\n" | rm /tmp/root/*.* 2> /dev/null
 echo '.........................................'
 printf "\033[40;37m\ngive me the files to include in main root ? "
 read a
 for b in $a
 do
-    cp $b ./root/
+    cp $b /tmp/root/
 done
 echo '.........................................'
 
 printf "\033[40;37m\ngive me iso image name ? "
 read h
-chmod 777 ./tmp/root/*
+chmod 777 /tmp/root/*
 echo '.........................................'
-cp isolinux.bin ./tmp/root 2> /dev/null
-cp isolinux.cfg ./tmp/root 2> /dev/null
-cp vmlinuz ./tmp/root 2> /dev/null
-cp initrd.gz ./tmp/root 2> /dev/null
-chmod 777 ./tmp/root/*
+cp isolinux.bin /tmp/root 2> /dev/null
+cp isolinux.cfg /tmp/root 2> /dev/null
+cp vmlinuz /tmp/root 2> /dev/null
+cp initrd.gz /tmp/root 2> /dev/null
+chmod 777 /tmp/root/*
 
-genisoimage -o "$h" -input-charset utf-8 -b "isolinux.bin" -no-emul-boot -boot-load-size 4  -boot-info-table "./root"
+genisoimage -o "$h" -input-charset utf-8 -b "isolinux.bin" -no-emul-boot -boot-load-size 4  -boot-info-table "/tmp/root"
 chmod 777 $h
